b79066cbc14d393b6a848e7fe28a17db11009457
Soumya <kantipudisoumya@yahoo.com>
2026-03-28 16:43:10 -0700

    added options for browser

--- src/main/java/factory/BrowserFactory.java
+++ src/main/java/factory/BrowserFactory.java
@@ -2,30 +2,56 @@ package factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import utilities.ConfigReader;
 
 public class BrowserFactory {
    public static WebDriver initBrowser(String browserType) {
+	  
+	  
 	  WebDriver driver=null;
       if (browserType == null || browserType.trim().isEmpty()) {
     	  browserType=ConfigReader.getProperty("defaultBrowser");
       }
          switch(browserType.toLowerCase()) {
          case "chrome":
-               driver=new ChromeDriver();
+        	   ChromeOptions options=new ChromeOptions();
+        	   options.addArguments("--headless=new");
+        	   options.addArguments("--no-sandbox");
+        	   options.addArguments("--disable-dev-shm-usage");
+        	   options.addArguments("--disable-gpu");
+        	   options.addArguments("--window-size=1920,1080");
+               driver=new ChromeDriver(options);
                break;
          case "firefox":
-               driver=new FirefoxDriver();
+               FirefoxOptions options_ff=new FirefoxOptions();
+        	   options_ff.addArguments("--headless=new");
+        	   options_ff.addArguments("--width=1920");
+        	   options_ff.addArguments("--height=1080");
+        	   driver=new FirefoxDriver(options_ff);
                break;
          case "edge":
-            driver=new EdgeDriver();
+        	EdgeOptions options_edge=new EdgeOptions();
+        	options_edge.addArguments("--headless=new");
+     	    options_edge.addArguments("--no-sandbox");
+     	    options_edge.addArguments("--disable-dev-shm-usage");
+     	    options_edge.addArguments("--window-size=1920,1080");
+            driver=new EdgeDriver(options_edge);
             break;
          case "default":
-            driver=new ChromeDriver();
-            break;
+        	 ChromeOptions options_c=new ChromeOptions();
+      	     options_c.addArguments("--headless=new");
+      	     options_c.addArguments("--no-sandbox");
+      	     options_c.addArguments("--disable-dev-shm-usage");
+      	     options_c.addArguments("--disable-gpu");
+      	     options_c.addArguments("--window-size=1920,1080");
+             driver=new ChromeDriver(options_c);
+             break;
          }
       return driver;
    }
